0aabe5d1396537c662c245a6647d74755967955f
Bernardo Santos <bphbs@iscte-iul.pt>
2020-11-14 22:09:34 +0000

    14/11/2020

--- sokoban/Sokoban_92586_92439_CatarinaMoreira_BernardoSantos/src/pt/iscte/dcti/poo/sokoban/starter/ActiveObjects.java
+++
@@ -1,9 +0,0 @@
-package pt.iscte.dcti.poo.sokoban.starter;
-
-import pt.iul.ista.poo.utils.Point2D;
-
-public interface ActiveObjects {
-	
-	public void move(Point2D position);
-	
-}
--- sokoban/Sokoban_92586_92439_CatarinaMoreira_BernardoSantos/src/pt/iscte/dcti/poo/sokoban/starter/InteractiveObjects.java
+++
@@ -1,7 +0,0 @@
-package pt.iscte.dcti.poo.sokoban.starter;
-
-public interface InteractiveObjects {
-	
-	public void interact(AbstractObjects object);
-	
-}
